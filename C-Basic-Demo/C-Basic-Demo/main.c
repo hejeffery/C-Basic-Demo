bf7f2f40f35de2043e3a0ac3c0ed8c5a0a315f28
hejeffery <553504116@qq.com>
2017-01-17 13:52:41 +0800

    完善demo

--- C-Basic-Demo/C-Basic-Demo/main.c
+++ C-Basic-Demo/C-Basic-Demo/main.c
@@ -649,6 +649,7 @@ void bitOperation() {
 
 // 一些数组容易混淆的知识点
 void someArrayConfusion() {
+    printf("*********一维数组*********\n");
     // 一维数组
     int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     // a和&a的区别
@@ -659,6 +660,7 @@ void someArrayConfusion() {
     // 下面的+1就是加上一个类型的大小
     printf("a + 1 = %p, &a + 1 = %p\n", a + 1, &a + 1);
     
+    printf("\n*********二维数组*********\n");
     // 二维数组
     int b[3][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
     // b、&b，*b的区别
@@ -667,6 +669,43 @@ void someArrayConfusion() {
     // 2.&b是数组的地址，指向一个二维数组。此时可以把数组当成一个类型，改类型的大小是4(int是4个字节) * 3 * 4，所以&b + 1代表移动该数组类型的1个长度，在此也就是移动48字节
     // 3.*b是首行首列的地址，相当于列指针，*b + 1往后移动一个元素，就是首行第1列的地址
     printf("b + 1= %p, &b + 1 = %p, *b + 1 = %p\n", b + 1, &b + 1, *b + 1);
+    
+    // 数组指针
+    printf("\n*********数组指针*********\n");
+    int (*p)[10];
+    // 可以判断这里的p的类型是数组
+    printf("p = %p, p + 1 = %p", p, p + 1);
+}
+
+// 二维数组和指针
+void twoDimensionalArray() {
+    int array[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%p, %2d, ", &array[i][j], array[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\n");
+    
+    printf("\n*********打印行*********\n");
+    for (int i = 0; i < 4; i++) {
+        printf("%p, %2d\n", array + i, *array[i]);
+    }
+    
+    printf("\n*********打印列*********\n");
+    for (int i = 0; i < 4; i++) {
+        printf("%p, %2d\n", *array + i, *(*array + i));
+    }
+    printf("\n");
+    
+    // 二维数组总结：
+    // array是行指针；*(array + i) + j是列指针
+    // array + i === &array[i]，表示第i行
+    // *(array + i) === array[i] 表示第i行第0列的地址
+    // *(array + i) + j === array[i] + j 表示第i行第j列的地址
+    // *(*(array + i) + j) === *(array[i] + j) === array[i][j] 表示第i行第j列
 }
 
 int main(int argc, const char * argv[]) {
@@ -798,6 +837,9 @@ int main(int argc, const char * argv[]) {
     
 //    // 一些数组容易混淆的知识点
 //    someArrayConfusion();
+    
+    // 二维数组和指针
+    twoDimensionalArray();
     
     return 0;
 }
