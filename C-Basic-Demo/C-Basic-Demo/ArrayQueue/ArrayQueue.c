9f6f108219d0eed046c3f871f58681a40a39f60e
hejeffery <553504116@qq.com>
2017-01-21 10:09:48 +0800

    调整

--- C-Basic-Demo/C-Basic-Demo/ArrayQueue/ArrayQueue.c
+++ C-Basic-Demo/C-Basic-Demo/ArrayQueue/ArrayQueue.c
@@ -16,20 +16,20 @@ void createQueue(Queue *queue) {
 }
 
 bool isEmptyQueue(Queue *queue) {
-    // 如果队尾的值为0，说明该队列是空队列
-    return queue->tail == 0 ? true : false;
+    // 如果队头的值为0，说明该队列是空队列
+    return queue->head == 0 ? true : false;
 }
 
 bool isFullQueue(Queue *queue) {
-    // 如果队尾的值是QueueLength - 1，说明该队列已满
-    return queue->tail == QueueLength - 1 ? true : false;
+    // 如果队头的值是QueueLength - 1，说明该队列已满
+    return queue->head == QueueLength - 1 ? true : false;
 }
 
 bool enqueue(Queue *queue, int value) {
     // 如果是空队列
     if (isEmptyQueue(queue)) {
-        queue->data[queue->head] = value;
-        queue->tail++;
+        queue->data[queue->tail] = value;
+        queue->head++;
         return true;
 
     } else {
@@ -39,13 +39,13 @@ bool enqueue(Queue *queue, int value) {
 
         } else {// 队列没满
             // 所有的数据往后移动一位
-            for (int i = queue->tail; i > 0; i--) {
+            for (int i = queue->head; i > 0; i--) {
                 queue->data[i] = queue->data[i - 1];
             }
-            // 队尾的index自增
-            queue->tail++;
-            // 队头赋值为新值
-            queue->data[queue->head] = value;
+            // 队头的index自增
+            queue->head++;
+            // 队尾赋值为新值
+            queue->data[queue->tail] = value;
             return true;
         }
     }
@@ -59,9 +59,9 @@ bool dequeue(Queue *queue, int *value) {
     }
     
     // 赋值出队的数据
-    *value = queue->data[queue->tail - 1];
-    // 队尾自减
-    queue->tail--;
+    *value = queue->data[queue->head - 1];
+    // 队头自减
+    queue->head--;
     return true;
 }
 
@@ -69,14 +69,14 @@ int headElement(Queue *queue) {
     if (isEmptyQueue(queue)) {
         return -1;
     }
-    return queue->data[queue->head];
+    return queue->data[queue->head - 1];
 }
 
 int tailElement(Queue *queue) {
     if (isEmptyQueue(queue)) {
         return -1;
     }
-    return queue->data[queue->tail - 1];
+    return queue->data[queue->tail];
 }
 
 void showQueueData(Queue *queue) {
@@ -85,7 +85,7 @@ void showQueueData(Queue *queue) {
     }
     
     printf("=================队列数据遍历开始==================\n");
-    for (int i = 0; i < queue->tail; i++) {
+    for (int i = 0; i < queue->head; i++) {
         printf("%d  \n", queue->data[i]);
     }
     printf("=================队列数据遍历结束==================\n");
