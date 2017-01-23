1b802dae83865c5a17df1389af1a17be6ad0b40a
hejeffery <553504116@qq.com>
2017-01-23 10:24:15 +0800

    调整

--- C-Basic-Demo/C-Basic-Demo/C_Algorithm/Algorithm.h
+++ C-Basic-Demo/C-Basic-Demo/C_Algorithm/Algorithm.h
@@ -17,4 +17,54 @@
 //                                                                                //
 ////////////////////////////////////////////////////////////////////////////////////
 
+/**
+ *  选择排序
+ *
+ *  @param array 需要排序的数组
+ *  @param count 数组的个数
+ */
+void selectionSort(int *array, int count);
+
+/**
+ *  冒泡排序
+ *
+ *  @param array 需要排序的数组
+ *  @param count 数组的个数
+ */
+void bubbleSort(int *array, int count);
+
+/**
+ *  插入排序
+ *
+ *  @param array 需要排序的数组
+ *  @param count 数组的个数
+ */
+void insertSort(int *array, int count);
+
+/**
+ *  二分查找(折半查找)
+ *
+ *  @param array 需要查找的数组
+ *  @param count 数组的个数
+ *  @param searchNumber 查找的元素
+ */
+int binarySearch(int *array, int count, int searchNumber);
+
+/**
+ *  拉格朗日查找(二分查找的通用模式)
+ *
+ *  @param array 需要查找的数组
+ *  @param count 数组的个数
+ *  @param searchNumber 查找的元素
+ */
+int lagrangeSearch(int *array, int count, int searchNumber);
+
+/**
+ *  使一个有序的数组乱序
+ *
+ *  @param array 需要乱序的数组
+ *  @param count 数组的个数
+ */
+void outOfOrderArray(int *array, int count);
+
 #endif /* Algorithm_h */
--- C-Basic-Demo/C-Basic-Demo/main.c
+++ C-Basic-Demo/C-Basic-Demo/main.c
@@ -12,6 +12,7 @@
 #include <time.h>
 #include <math.h>
 
+#include "Algorithm.h"
 #include "ArrayStack.h"
 #include "ArrayQueue.h"
 #include "SequenceArrayList.h"
@@ -88,128 +89,6 @@ void nineMultNine() {
             printf("%d*%d = %2d   ", i, j, i * j);
         }
         printf("\n");
-    }
-}
-
-// 选择排序
-void selectionSort(int *array, int count) {
-    // 选择排序法的思想：每一趟都是找出最值(和找出一个数组中的最值算法类似)，然后交换。
-    // 也就是第一个数和后面的数逐个扫描，第二个数和后面的数逐个扫描....
-    for (int i = 0; i < count; i++) {
-        for (int j = i + 1; j < count; j++) {
-            if (array[i] < array[j]) {
-                array[i] = array[i] ^ array[j];
-                array[j] = array[i] ^ array[j];
-                array[i] = array[i] ^ array[j];
-            }
-        }
-    }
-    
-    for (int i = 0; i < count; i++) {
-        printf("%d  \n", array[i]);
-    }
-}
-
-// 冒泡排序
-void bubbleSort(int *array, int count) {
-    // 冒泡排序的思想：数字上浮或者下沉
-    int loopNumber = count - 1;
-    for (int i = 0; i < loopNumber; i++) {// 每循环一次就有一个数上浮或者下沉
-        for (int j = 0; j < loopNumber - i; j++) {
-            if (array[j] > array[j + 1]) {
-                array[j] = array[j] ^ array[j + 1];
-                array[j + 1] = array[j] ^ array[j + 1];
-                array[j] = array[j] ^ array[j + 1];
-            }
-        }
-    }
-    for (int i = 0; i < count; i++) {
-        printf("%d  \n", array[i]);
-    }
-}
-
-// 插入排序
-void insertSort(int *array, int count) {
-    // 插入排序的思想：把数组分为n-1个数组序列。每次从数组的最后一个值往前插值
-    // 从第二个元素开始
-    for (int i = 1; i < count; i++) {
-        // 用来记录数组序列的最后一个元素的位置
-        int index = i;
-        // 用来记录数组序列的最后一个元素
-        int element = array[i];
-        
-        while (index > 0 && element < array[index - 1]) {
-            array[index] = array[index - 1];// 往后移
-            index--;
-        }
-        array[index] = element;
-    }
-    
-    for (int i = 0; i < count; i++) {
-        printf("%d  \n", array[i]);
-    }
-}
-
-// 二分查找(折半查找)
-int binarySearch(int *array, int count, int searchNumber) {
-    int headIndex = 0;
-    int tailIndex = count - 1;
-    while (headIndex <= tailIndex) {
-        int middleIndex = (headIndex + tailIndex) / 2;
-        if (searchNumber < array[middleIndex]) {// 从上半部分查找
-            tailIndex = middleIndex - 1;
-            
-        } else if (searchNumber > array[middleIndex]) {// 从下半部分查找
-            headIndex = middleIndex + 1;
-            
-        } else if (searchNumber == array[middleIndex]) {// 找到了
-            return middleIndex;
-            
-        }
-    }
-    return -1;// 没有找到返回-1
-}
-
-// 拉格朗日查找(二分查找的通用模式)
-int lagrangeSearch(int *array, int count, int searchNumber) {
-    int headIndex = 0;
-    int tailIndex = count - 1;
-    while (headIndex <= tailIndex) {
-        // 比例
-        float ratio = ((searchNumber - array[headIndex]) / (array[tailIndex] - array[headIndex])) * 1.0;
-        int middleIndex = headIndex + (tailIndex - headIndex) * ratio;
-        if (searchNumber < array[middleIndex]) {// 从上半部分查找
-            tailIndex = middleIndex - 1;
-            
-        } else if (searchNumber > array[middleIndex]) {// 从下半部分查找
-            headIndex = middleIndex + 1;
-            
-        } else if (searchNumber == array[middleIndex]) {// 找到了
-            return middleIndex;
-            
-        }
-    }
-    return -1;// 没有找到返回-1
-}
-
-// 使一个有序的数组乱序
-void outOfOrderArray(int *array, int count) {
-    // 获取随机数种子
-    // 思想：1.第1个元素和第1个元素后面的9个元素中的其中一个交换
-    //      2.第2个元素和第2个元素后面的8个元素中的其中一个交换
-    //      3.第3个元素和第3个元素后面的7个元素中的其中一个交换......
-    time_t t;
-    srand((unsigned int)time(&t));
-    
-    for (int i = 0; i < count - 1; i++) {
-        int randomIndex = i + (rand() % (count - 1 - i));
-        int temp = array[i];
-        array[i] = array[randomIndex];
-        array[randomIndex] = temp;
-    }
-    
-    for (int i = 0; i < count; i++) {
-        printf("%d  \n", array[i]);
     }
 }
 
@@ -1008,8 +887,8 @@ int main(int argc, const char * argv[]) {
 //    // 动态数组队列
 //    dynamicQueueImpl();
     
-    // 动态数组栈
-    dynamicStackImpl();
+//    // 动态数组栈
+//    dynamicStackImpl();
     
 //    // 用动态分配内存打印
 //    dynamicPrintArray();
