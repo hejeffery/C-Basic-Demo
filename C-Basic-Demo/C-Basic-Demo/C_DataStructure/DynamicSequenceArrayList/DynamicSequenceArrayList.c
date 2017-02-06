419ea5c26bb00c58462a446833e9e7bc1fc4057d
hejeffery <553504116@qq.com>
2017-02-06 17:42:34 +0800

    完善demo

---
+++ C-Basic-Demo/C-Basic-Demo/C_DataStructure/DynamicSequenceArrayList/DynamicSequenceArrayList.c
@@ -0,0 +1,9 @@
+//
+//  DynamicSequenceArrayList.c
+//  C-Basic-Demo
+//
+//  Created by HeJeffery on 2017/2/6.
+//  Copyright © 2017年 HeJeffery. All rights reserved.
+//
+
+#include "DynamicSequenceArrayList.h"
---
+++ C-Basic-Demo/C-Basic-Demo/C_DataStructure/DynamicSequenceArrayList/DynamicSequenceArrayList.h
@@ -0,0 +1,20 @@
+//
+//  DynamicSequenceArrayList.h
+//  C-Basic-Demo
+//
+//  Created by HeJeffery on 2017/2/6.
+//  Copyright © 2017年 HeJeffery. All rights reserved.
+//
+
+#ifndef DynamicSequenceArrayList_h
+#define DynamicSequenceArrayList_h
+
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////////
+//                                                                                //
+//                            数组的顺序存储，动态数组                                 //
+//                                                                                //
+////////////////////////////////////////////////////////////////////////////////////
+
+#endif /* DynamicSequenceArrayList_h */
