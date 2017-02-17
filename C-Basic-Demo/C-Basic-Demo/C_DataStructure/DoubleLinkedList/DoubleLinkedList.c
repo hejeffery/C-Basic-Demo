c9683b29bda12eda5f4fcf0360043646ffefe6b3
hejeffery <553504116@qq.com>
2017-02-17 18:09:03 +0800

    添加DoubleLinkedList

---
+++ C-Basic-Demo/C-Basic-Demo/C_DataStructure/DoubleLinkedList/DoubleLinkedList.c
@@ -0,0 +1,9 @@
+//
+//  DoubleLinkedList.c
+//  C-Basic-Demo
+//
+//  Created by HeJeffery on 2017/2/17.
+//  Copyright © 2017年 HeJeffery. All rights reserved.
+//
+
+#include "DoubleLinkedList.h"
---
+++ C-Basic-Demo/C-Basic-Demo/C_DataStructure/DoubleLinkedList/DoubleLinkedList.h
@@ -0,0 +1,20 @@
+//
+//  DoubleLinkedList.h
+//  C-Basic-Demo
+//
+//  Created by HeJeffery on 2017/2/17.
+//  Copyright © 2017年 HeJeffery. All rights reserved.
+//
+
+#ifndef DoubleLinkedList_h
+#define DoubleLinkedList_h
+
+#include <stdio.h>
+
+////////////////////////////////////////////////////////////////////////////////////
+//                                                                                //
+//                                        双链表                                   //
+//                                                                                //
+////////////////////////////////////////////////////////////////////////////////////
+
+#endif /* DoubleLinkedList_h */
